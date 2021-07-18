ce678cd4971d0fd729e71412cdea33e13b8d8627
Rossana Grizzo <rossana.grizzo@gmail.com>
2021-07-18 11:37:47 +0200

    modified backend

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,120 @@
 "use strict";
+// PER FAR FUNZIONARE, ESEGUIRE QUESTI COMANDI SU UN NUOVO TERMINALE (`CTRL` + `SHIFT` + `ò`) ⬇⬇⬇
 exports.__esModule = true;
+// npm i fastify
+// npm i fastify-swagger
+// npm i nodemon
+// npm i @types/node
+// POI, SEMPRE SU UN NUOVO TERMINALE (`CTRL` + `SHIFT` + `ò`) SCRIVERE : ` nodemon . `
+// POI, SEMPRE SU UN NUOVO TERMINALE (`CTRL` + `SHIFT` + `ò`) SCRIVERE : ` tsc -w index.ts `
+// OPLA', IL GIOCO E' FATTO, ANDATE SUL BROWSER E SCRIVETE SULLA BARRA DEGLI INDIRIZZI  ` http://127.0.0.1:3000/api `  || NOTA BENE: SE SI CAMBIA RIGA 29 (routePrefix: '/api',), NON SI DOVRA' PIU METTERE  ..1:3000/API MA ..1:3000/PAROLA-CHE-HAI-SCRITTO-AL-POSTO-DI-API
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////                                                                                                         //
+///  PREMERE `CTRL` + `F` E DIGITARE `###`, QUELLI SONO I CAMPI DA MODIFICARE IN RELAZIONE ALL'ESERCIZIO    ///
+//                                                                                                         ////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var fastify_1 = require("fastify");
-var fastify_swagger_1 = require("fastify-swagger");
+var swagger = require("fastify-swagger");
 var app = fastify_1["default"]({
     logger: true,
     ignoreTrailingSlash: true
 });
-app.register(fastify_swagger_1["default"], {
-    routePrefix: '/documentation',
+app.register(swagger["default"], {
+    routePrefix: '/api',
     swagger: {
         info: {
-            title: 'prima prova',
-            description: 'test fastify api',
-            version: '0.1.0'
+            title: '###Esame ITS 2021',
+            description: '###Api Esame',
+            version: '1.0.0'
         },
-        host: 'localhost:3000',
+        host: '127.0.0.1:3000',
         schemes: ['http'],
         consumes: ['application/json'],
         produces: ['application/json']
     },
     exposeRoute: true
 });
-app.listen(3000, function (err, address) {
+// CHIAMATA GET PER RICEVERE I DATI
+app.get("/pippo", function (req, reply) {
+    reply
+        .code(200) // => IL CODICE CHE VIENE TORNATO
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({
+        name: '###NOME',
+        theme: '###THEME',
+        attraction: '###ATTRACTION',
+        location: '###LOCATION' //  |  =>
+    });
+});
+// CHIAMATA POST PER INSERIRE I DATI
+app.post("/pippo", {
+    schema: {
+        params: {
+            type: "object",
+            properties: {
+                nome: {
+                    type: "string",
+                    description: "###Inserire descrizione chiamata POST (Ex: 'Inserire Nome')"
+                },
+                cognome: {
+                    type: "string",
+                    description: "###Inserire descrizione chiamata POST"
+                },
+                numero: {
+                    type: "number",
+                    description: "###Inserire descrizione chiamata POST"
+                }
+            }
+        }
+    }
+}, function (req, reply) {
+    reply
+        .code(200) // => IL CODICE CHE VIENE TORNATO
+        .send({
+        message: "###Data added succesfully!"
+    });
+});
+// CHIAMATA PUT PER MODIFICARE I DATI
+app.put("/pippo/:name", {
+    schema: {
+        params: {
+            type: "object",
+            properties: {
+                nome: {
+                    type: "string",
+                    description: "###Inserire descrizione chiamata POST (Ex: 'Inserire Nome da cambiare')"
+                }
+            }
+        }
+    }
+}, function (req, reply) {
+    reply
+        .code(200) // => IL CODICE CHE VIENE TORNATO
+        .send({
+        message: "###Data updated"
+    });
+});
+// CHIAMATA DELETE PER ELIMINARE I DATI
+app["delete"]("/pippo/:name", {
+    schema: {
+        params: {
+            type: "object",
+            properties: {
+                nome: {
+                    type: "string",
+                    description: "###Inserire descrizione chiamata POST (Ex: 'Inserire Nome dell'utente da eliminare')"
+                }
+            }
+        }
+    }
+}, function (req, reply) {
+    reply
+        .code(200) // => IL CODICE CHE VIENE TORNATO
+        .send({
+        message: "###User deleted succesfully!"
+    });
+});
+app.listen(3000, function (err) {
     if (err)
         throw err;
-    app.log.info('server listening on' + address);
 });
--- backend/index.ts
+++ backend/index.ts
@@ -1,25 +1,135 @@
-import fastify from "fastify";
-import swagger from 'fastify-swagger'
+
+// PER FAR FUNZIONARE, ESEGUIRE QUESTI COMANDI SU UN NUOVO TERMINALE (`CTRL` + `SHIFT` + `ò`) ⬇⬇⬇
+
+// npm i fastify
+// npm i fastify-swagger
+// npm i nodemon
+// npm i @types/node
+
+// POI, SEMPRE SU UN NUOVO TERMINALE (`CTRL` + `SHIFT` + `ò`) SCRIVERE : ` nodemon . `
+// POI, SEMPRE SU UN NUOVO TERMINALE (`CTRL` + `SHIFT` + `ò`) SCRIVERE : ` tsc -w index.ts `
+
+// OPLA', IL GIOCO E' FATTO, ANDATE SUL BROWSER E SCRIVETE SULLA BARRA DEGLI INDIRIZZI  ` http://127.0.0.1:3000/api `  || NOTA BENE: SE SI CAMBIA RIGA 29 (routePrefix: '/api',), NON SI DOVRA' PIU METTERE  ..1:3000/API MA ..1:3000/PAROLA-CHE-HAI-SCRITTO-AL-POSTO-DI-API
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////                                                                                                         //
+///  PREMERE `CTRL` + `F` E DIGITARE `###`, QUELLI SONO I CAMPI DA MODIFICARE IN RELAZIONE ALL'ESERCIZIO    ///
+//                                                                                                         ////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import fastify from 'fastify'
+import * as swagger from 'fastify-swagger'
+
 const app = fastify({
-    logger: true,
-    ignoreTrailingSlash: true
-});
-app.register(swagger, {
-    routePrefix: '/documentation',
-    swagger: {
-        info: {
-            title: 'prima prova',
-            description: 'test fastify api',
-            version: '0.1.0'
-        },
-        host: 'localhost:3000',
-        schemes: ['http'],
-        consumes: ['application/json'],
-        produces: ['application/json']
+  logger: true,
+  ignoreTrailingSlash: true
+})
+
+app.register(swagger.default, {
+  routePrefix: '/api',
+  swagger: {
+    info: {
+      title: '###Esame ITS 2021',
+      description: '###Api Esame',
+      version: '1.0.0'
     },
-    exposeRoute: true
-});
-app.listen(3000,(err, address) =>{
-    if (err) throw err
-    app.log.info('server listening on' + address)
-});
+    host: '127.0.0.1:3000',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json']
+  },
+  exposeRoute: true
+})
+
+// CHIAMATA GET PER RICEVERE I DATI
+app.get("/pippo", (req, reply) => {//###CAMBIA PIPPO!
+  reply
+    .code(200)  // => IL CODICE CHE VIENE TORNATO
+    .header('Content-Type', 'application/json; charset=utf-8')
+    .send(
+      { 
+        name: '###NOME',                    //  |  =>
+        theme: '###THEME' ,                 //  |  =>  I CAMPI CHE VERRANNO TORNATI CON LA CHIAMATA `GET`
+        attraction: '###ATTRACTION',        //  |  =>  PUOI ANCHE TORNARE UN ARRAY DI DATI VOLENDO, SAREBBE PIU' 'REALE'
+        location: '###LOCATION'             //  |  =>
+      }
+    )
+})
+
+// CHIAMATA POST PER INSERIRE I DATI
+app.post("/pippo", {//###CAMBIA PIPPO!
+    schema: { 
+      params: {
+        type: "object",
+        properties: {
+          nome: {
+            type: "string",
+            description: "###Inserire descrizione chiamata POST (Ex: 'Inserire Nome')"
+          },
+          cognome: {
+            type: "string",
+            description: "###Inserire descrizione chiamata POST"
+          },
+          numero: {
+            type: "number",  // esempio aggiunta di un numero
+            description: "###Inserire descrizione chiamata POST"
+          }
+        }
+      } 
+    }
+  }, (req, reply) => {
+    reply
+      .code(200)  // => IL CODICE CHE VIENE TORNATO
+      .send({
+        message: "###Data added succesfully!"
+      })
+  }
+)
+
+// CHIAMATA PUT PER MODIFICARE I DATI
+app.put("/pippo/:name", {
+  schema: { 
+    params: {
+      type: "object",
+      properties: {
+        nome: {
+          type: "string",
+          description: "###Inserire descrizione chiamata POST (Ex: 'Inserire Nome da cambiare')"
+        }
+      }
+    } 
+  }
+}, (req, reply) => {//###CAMBIA PIPPO!
+  reply
+    .code(200)  // => IL CODICE CHE VIENE TORNATO
+    .send({
+      message: "###Data updated"
+    })
+  }
+)
+
+// CHIAMATA DELETE PER ELIMINARE I DATI
+app.delete("/pippo/:name", {
+  schema: { 
+    params: {
+      type: "object",
+      properties: {
+        nome: {
+          type: "string",
+          description: "###Inserire descrizione chiamata POST (Ex: 'Inserire Nome dell'utente da eliminare')"
+        }
+      }
+    } 
+  }
+}, (req, reply) => {//###CAMBIA PIPPO!
+  reply
+    .code(200)  // => IL CODICE CHE VIENE TORNATO
+    .send({
+      message: "###User deleted succesfully!"
+    })
+  }
+)
+
+app.listen(3000, err => {
+  if (err) throw err
+})
